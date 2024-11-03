a7c24ac4759e9b920776322f8ee8fde6bcfc3087
Fahmi Aziz <angkatandualima25@gmail.com>
2024-11-03 20:36:51 +0800

    first commit

---
+++ 00-template/main.py
@@ -0,0 +1,11 @@
+print("HELLO WORLD")
+
+nama = "Fahmi Aziz"
+umur = 20
+print("Nama:", nama)
+print("Umur:", umur)
+
+def sapa(pesan):
+    print("Pesan:", pesan)
+
+sapa("-Wellcome To My Domain Expansion-")
