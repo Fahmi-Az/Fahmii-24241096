0e2aaeda4aed950920c490b608a9cd51c2c8ab5f
Fahmi Aziz <angkatandualima25@gmail.com>
2024-11-05 17:31:31 +0800

    modul

--- 04-InputUser/main.py
+++ 04-InputUser/main.py
@@ -9,4 +9,18 @@ angka = float (input("masukan angka : "))
 print("data : ", angka, " type = ", type(angka))
 
 # bagaimana jika data adalah boolean?
+# jika data adalah boolean
+boolean_input = input("masukan boolean (True/False) : ")
+
+# konversi input ke boolean
+if boolean_input == "True":
+    boolean_data = True
+elif boolean_input == "False":
+    boolean_data = False
+
+# jika input tidak valid
+else:
+    boolean_data = None
+
+print("data : ", boolean_data, "type = ", type(boolean_data))
 
