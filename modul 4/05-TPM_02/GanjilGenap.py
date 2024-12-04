a53b2650efd29dd8f196b9a59a6a1d90bee10f06
Fahmi-Az <angkatandualima25@gmail.com>
2024-12-04 16:21:09 +0800

    Delete modul 4 directory

--- modul 4/05-TPM_02/GanjilGenap.py
+++
@@ -1,9 +0,0 @@
-# Input bilangan
-angka = float(input("Masukkan bilangan: "))
-
-# Cek ganjil atau genap
-# f = formatted string berfungsi untuk menyisipkan variabel langsung ke dalam string dengan cara yang lebih sederhana
-if angka % 2 == 0:
-    print(f"{angka} adalah bilangan genap")
-else:
-    print(f"{angka} adalah bilangan ganjil")
